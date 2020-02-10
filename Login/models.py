579f80c37ce87c9bb660cb658e5a60a19f9f1de5
DevOsuna <183462@ids.upchiapas.edu.mx>
2020-02-10 09:32:20 -0600

    Model Login

--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.util import timezone
+
+class Exmaple2(models.Model):
+    name = models.CharField(max_length=254, null=False)
+    year = models.IntegerField(null=False)
+    delete = models.BooleanField(default=False)
+    create = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+    class meta:
+        db_table = 'Example2'
 
 # Create your models here.
