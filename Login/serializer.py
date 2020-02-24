de4701fb717af32dbf537b3b657ee9151c7d6ce3
DevOsuna <183462@ids.upchiapas.edu.mx>
2020-02-23 18:17:48 -0600

    Se actualizo gitignore

--- Login/serializer.py
+++
@@ -1,9 +0,0 @@
-# --------------AGRAGANDO LIBERIAS FRAMEWORK --------------
-from rest_framework import routers, serializers, viewsets
-
-from Login.models import Example2
-
-class Example2Serializers(serializers.ModelSerializer):
-    class Meta:
-        model = Example2
-        fields = ('__all__')
