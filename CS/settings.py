68cdfe01380a66f878b9cc502960babfe24487b8
DevOsuna <183462@ids.upchiapas.edu.mx>
2020-03-15 23:19:57 -0600

    Staticfiles_root en setting.py

--- CS/settings.py
+++ CS/settings.py
@@ -154,5 +154,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = "/home/cesar/ClienteServidorOsuna/media/;"
+STATIC_ROOT = "/home/cesar/ClienteServidorOsuna/assets/"
 STATIC_URL = '/static/'
